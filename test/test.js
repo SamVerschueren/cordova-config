24989a80303390ef3fbc3d7a6ab776cabeba8539
gburnett <garyburnett3000@gmail.com>
2015-10-05 21:39:10 +0100

    Add tests for setElement

--- test/test.js
+++ test/test.js
@@ -108,6 +108,51 @@ describe('cordova-config', function() {
         });
     });
 
+    describe('#setElement', function () {
+        it('Should add an empty element if it does not yet exist', function () {
+            // Load the config and set the element
+            var config = new Config(__dirname + '/fixtures/config.empty.xml');
+            config.setElement('earth');
+
+            // Test
+            var element = config._doc.find('./earth');
+            element.tag.should.be.equal('earth');
+        });
+
+        it('Should add an element, including text, if it does not yet exist', function () {
+            // Load the config and set the element
+            var config = new Config(__dirname + '/fixtures/config.empty.xml');
+            config.setElement('earth', 'heavy matter');
+
+            // Test
+            var element = config._doc.find('./earth');
+            element.tag.should.be.equal('earth');
+            element.text.should.be.equal('heavy matter');
+        });
+
+        it('Should update attributes of an existing element', function () {
+            // Load the config and set the element
+            var config = new Config(__dirname + '/fixtures/config.xml');
+            config.setElement('content', '', { src: 'material.html' });
+
+            // Test
+            var element = config._doc.find('./content');
+            element.tag.should.be.equal('content');
+            element.attrib.src.should.be.equal('material.html');
+        });
+
+        it('Should update text of an existing element', function () {
+            // Load the config and set the element
+            var config = new Config(__dirname + '/fixtures/config.xml');
+            config.setElement('Description', 'A statement or an account describing something');
+
+            // Test
+            var element = config._doc.find('./Description');
+            element.tag.should.be.equal('Description');
+            element.text.should.be.equal('A statement or an account describing something');
+        });
+    });
+
     describe('#setDescription', function() {
 
         it('Should add a description tag if it does not yet exist', function() {
